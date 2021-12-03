69bb1ee498e19d2de29b91035e0004aa62351a35
totherain155 <totherain155@gmail.com>
2021-12-03 15:38:31 +0900

    practin mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express"); // express module을 가져온다.
 const app = express(); // 새로운 express app을 만든다.
 const port = 5000;
 
+mongodb+srv://totherain:<password>@practicedb.egfhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 app.get("/", (req, res) => res.send("Hi!!"));
 
 // 5000 port에서 app을 실행한다.
